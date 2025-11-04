06a4e75e63916b93e8ef3b5ae2f38754a1ec0094
Shankavie Gnaneswaran <136223226+gshankavie@users.noreply.github.com>
2025-11-04 12:07:09 -0500

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -1,6 +1,8 @@
+# %%
 import sys
 import numpy as np
-import pandas as pd
+#import pandas as pd
+#print("hello")
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
@@ -12,11 +14,24 @@ from keras.models import Sequential
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, LeakyReLU
 
 
-print(f"TensorFlow Version: {tf.__version__}")
-print(f"Keras Version: {keras.__version__}")
+# %%
 
 
 
 # Step 1: data processing
 # define image shape and channel and batch size
 image_width, image_height, image_channel, batch_size = 500, 500, 3, 16;
+
+# %%
+# define image data directories
+training_data = r"Data\train"
+validation_data = r"Data\valid"
+
+# create training data image augmentation object
+train_data_aug = ImageDataGenerator(
+    rescale = 1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    #rotation_range = 40,
+    #horizontal_flip = True
+    )
